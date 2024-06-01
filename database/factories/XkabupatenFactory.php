5b81c6030dd10805b0ab12a7eebb610358ec4639
adiSM <vhanz.aja@gmail.com>
2024-06-01 14:32:38 +0700

    add parameter

---
+++ database/factories/XkabupatenFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class XkabupatenFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'created_at' => now(),
+            'updated_at' => now(),
+            'IsActive' => "1",
+        ];
+    }
+}
